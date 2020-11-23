dc3f17fec7d4a0ff473ede253db100b97e26ee5a
Kevin <kevin94.ka@gmail.com>
2020-11-23 14:29:22 -0600

    ds unzip, ds installation and restoring script uploaded

--- test.sh
+++ test.sh
@@ -1,1 +1,36 @@
-echo hello world
+unzip /fr/DS-6.5.4.zip
+
+bakDir=/fr/scriptsForMigration/bak
+
+
+echo Done Unzip
+#sleep 5
+
+
+B
+mv opendj amconfig
+
+echo moved amconfig 
+sleep 10 
+
+/fr/amconfig/setup directory-server \
+          --instancePath /fr/amconfig \
+          --rootUserDn cn=Directory\ Manager \
+          --rootUserPassword password \
+          --hostname localhost \
+          --adminConnectorPort 4444 \
+          --ldapPort 1389 \
+          --profile am-config:6.5.0 \
+          --set am-config/amConfigAdminPassword:password \
+	  --doNotStart \
+	  --acceptLicense
+
+
+#/fr/amconfig/bin/restore --offline --backupDirectory $bakDir/rootUser/	
+
+#/fr/amconfig/bin/restore --offline --backupDirectory $bakDir/cfgStore/
+
+#Restoring config file
+
+#unzip -d / scriptsForMigration/backup-config-1604430082.zip 
+
